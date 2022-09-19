d068f4d40c54025c4ce819e181974ceedb40eb56
mikemacfarland <m_macfarland@live.com>
2022-09-19 18:57:51 -0400

    updated about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,6 +6,15 @@ function About() {
     <div className='about'>
         <h1>About</h1>
         <p>Developed and Designed by <strong>Mike Macfarland</strong></p>
+        <p>This SPA currently uses the <strong><a href="https://developer.accuweather.com/">accuweather API</a></strong> to generate data.</p>
+
+        <ul className='about__list'>Future updates will include:
+          <li>List to select saved locations</li>
+          <li>Local or server storage for saved locations</li>
+          <li>account/email signup</li>
+          <li>push notifications</li>
+          
+        </ul>
         <Link className='__homeLink' to='/'>Back to Weather</Link>
     </div>
   )

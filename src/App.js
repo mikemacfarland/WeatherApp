5b992a9b335751a601d8fffa52b8a7aee8b164b9
mike macfarland <m_macfarland@live.com>
2023-01-03 12:04:43 -0500

    small styling changes, changed favicon

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import About from './pages/About';
 import Weather from './pages/Weather';
 import Footer from './components/Footer';
 
-// how make this app better
+// @TODO how make this app better
 //   make weather tabs openable and get hourly weather for a day
 //   redesign the IU ? 
 //   add display for night weather on day cards.
 //   add another route? for about page
 //   add dropdown list of saved locations (add delete)
 //   add not found display for zip code error
-//   export into mobile app
 
 function App() {
 
